be449f9a22ccacb09991745c552aafd559121655
Alejandro Sajaroff <asajaroff@users.noreply.github.com>
2021-06-18 01:38:36 -0300

    Adding software

--- software-install.sh
+++ software-install.sh
@@ -1,2 +1,30 @@
 #!/bin/bash
-sudo apt install nodejs golang -y
+
+# Programming languages
+sudo apt install nodejs golang make jq -y
+
+# Tools
+
+## yq
+VERSION=v4.9.6
+BINARY=yq_linux_arm
+wget https://github.com/mikefarah/yq/releases/download/${VERSION}/${BINARY}.tar.gz -O - |  tar xz && mv ${BINARY} /usr/bin/yq
+
+# kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.18.0/bin/linux/arm/kubectl
+
+# kops
+
+# helm
+
+# argocd
+
+# argo-rollouts
+
+# argo-workflows
+
+# awscli
+
+# azurecli
+
+# digitalocean
